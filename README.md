c3ea28551704b2f3a352f1c0ce8dcbcc67fe30a9
Prashu622 <145363935+Prashu622@users.noreply.github.com>
2025-08-09 23:16:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# 🌐 Animated Portfolio Website
+
+A sleek, responsive, and fully customizable **animated portfolio website** built using **React.js** and **Tailwind CSS**. T
+
+## 🚀 Live Demo
+
+🔗 [View Portfolio Live]
+
+## 🛠️ Tech Stack
+
+- ⚛️ React.js
+- 🎨 Tailwind CSS
+- 💾 Vercel(for deployment)
+
+## 📁 Features
+
+- Responsive design for all screen sizes 📱💻
+- Smooth scroll and animation effects ✨
+- Clean and modular React components 🧩
+- Easy to customize with Tailwind utility classes 🎯
+- Professional sections: About, Skills, Projects, Contact, etc. 👨‍💻
+
+
+## 🧑‍💻 How to Use
+
+### 1. Clone the Repo
+
+
+````
+
+### 2. Install Dependencies
+
+```bash
+npm install
+```
+
+### 3. Run the App
+
+```bash
+npm start
+```
+
+### 4. Build for Production
+
+```bash
+npm run build
+```
+
+
+
+## 🌟
+
+
+If you like it, feel free to ⭐ this repository and share!
---
+++ src/constants.js
@@ -0,0 +1,328 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/lzycrazy.png';
+import agcLogo from './assets/company_logo/Nit.jpg';
+import newtonschoolLogo from './assets/company_logo/codealpha.jpg';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/anduat.jpg';
+import vpsLogo from './assets/education_logo/up board.jpg';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/face.jpg';
+import csprepLogo from './assets/work_logo/jarvis.png';
+import movierecLogo from './assets/work_logo/face.png';
+import taskremLogo from './assets/work_logo/job finder.png';
+import npmLogo from './assets/work_logo/ATS.png';
+import webverLogo from './assets/work_logo/social.png';
+import cmLogo from './assets/work_logo/chatify.png';
+import imagesearchLogo from './assets/work_logo/pi learning.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "UI/UX Developer",
+      company: "LzyCrazy Pvt. Ltd.",
+      date: "August 2025 - Present",
+      desc: "I am Currently working as Graphics Designer in the LzyCrazy company basically this is the my part time job and i work the real project make it more attractive and deliver atttractive UI/UX for users smooth intracation.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Figma",
+        "Material UI",
+        "express.js",
+        "Tailwind CSS",
+        "Canva",
+        
+      
+      
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Research Intern",
+      company: "Motilal Nehru National Institute of Technology Allahabad",
+      date: "June 2025 - July 2025",
+      desc: " I am intern in the MNNIT- Prayagraj as a Research intern where i, Conducted research on IoT and machine learning applications, focusing on developing innovative solutions for real-time data processing and analysis. Collaborated with a team of researchers to design experiments, analyze results, and present findings in a comprehensive report.",
+      skills: [
+        "Python",
+        "IOT",
+        "Machine Learning",
+        "Deep Learning",
+        "Artificial Intelligence",
+        "CNN Algorithm",
+        "SQL",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Fullstack Developer ",
+      company: "Code Alpha",
+      date: "Mar 2024 - July2024",
+      desc: "Worked as a Full stack web devlopment Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "React JS",
+        "Figma",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+
+    {
+      id: 1,
+      img: bsaLogo,
+      Collage: "Acharya Narendra Deva University Of Agriculture And Technology, Ayodhya",
+      date: "Sept 2022 - Aug 2026",
+      grade: "7.4",
+      desc: "I completed my Bachelor's degree in Computer Science & Technology (B.tech) from Acharya Narendra Deva University Of Agriculture And Technology, Ayodhya. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at ANDUAT University allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Technology - B.tech(Computer Science & Engineering)",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Ashok Inter Collage, Chandauli, U.P.",
+      date: "Apr 2020 - March 2021",
+      grade: "78%",
+      desc: "I completed my class 12 education from Ashok Inter Collage, Chandauli, U.P, under the UP board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "UP BOARD(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: vpsLogo,
+      school: "Ashok Inter Collage, Chandauli, U.P.",
+      date: "Apr 2018 - March 2019",
+      grade: "73.5%",
+      desc: "I completed my class 10 education from Ashok Inter Collage, Chandauli, U.P, under the UP board, where I studied Science with Computer.",
+      degree: "UP BOARD(X), Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Face Recognition based Attendance System in the Raspberry Pi 4",
+      description:
+        "A powerful and user-friendly Face Recognition-based Attendance System built on Raspberry Pi 4. This project utilizes Python, Jupyter Notebook, and various machine learning and deep learning libraries to automate attendance tracking through facial recognition. It also includes an API for easy integration with other systems and supports IoT functionalities for real-time data processing.",
+      image: githubdetLogo,
+
+      tags: ["Python", "Juypter", "Machine Learning", "Deep Learning", "API", "IOT"],
+      github: "https://github.com/Prashu622/Face-Recognition-based-Attendance-System-in-the-Raspberry-pi-4",
+      
+    },
+    {
+      id: 1,
+      title: "Jarvis - A Personal Voice Assistant",
+      description:
+        "A personal voice assistant named Jarvis, built using Python. It can perform various tasks such as sending emails, fetching weather updates, and more, all through voice commands. The project showcases the integration of speech recognition and text-to-speech functionalities to create an interactive user experience.",
+      image: csprepLogo,
+      tags: ["Python", "Artificial Intelligence", "Machine Learning", "Open Ai", "Pyttx3", "Speech Recognition"],
+      github: "https://github.com/Prashu622",
+      
+    },
+    {
+      id: 2,
+      title: "Face Detection and Recognition System",
+      description:
+        "I made the fully working project which is Face Detection and Recognition using machine learning and deep learning and used the huge dataset for trained the model with 96% accuracy.",
+      image: movierecLogo,
+      tags: ["Python", "Juypter", "Machine Learning", "Deep Learning", "API", "CNN"],
+      github: "https://github.com/Prashu622",
+      
+    },
+    {
+      id: 3,
+      title: "Application Tracking System (PRASHUATS)",
+      description:
+        "An efficient and customizable Application tracking system. Built using React.js and Node.js, it provides robust validation features to help student for checking him resume for specific company and give the feedback for based on the its resume .",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation",  "API"],
+      github: "https://github.com/Prashu622",
+      webapp: "https://prashuats.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "JOB FINDER",
+      description:
+        "I made the fully deploy JOB Finder web page made using the react.js and javascript the motive of this project is connect students to the newly published job .",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS" ,"Express.js"],
+      github: "https://github.com/Prashu622",
+      
+    },
+    {
+      id: 5,
+      title: "Social Media Website",
+      description:
+        "I made social media app like facebook in this website you can create your account and share the any post and also chats with friends, and it is the very user friendly and clean design website.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Node.js", "Express.js", "MongoDB" , "API"],
+      github: "https://github.com/Prashu622",
+      
+    },
+    {
+      id: 6,
+      title: "CHATIFY",
+      description:
+        "An ed-tech platform where users can chat to each other and sendings the files over private web.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "REAL TIME CHAT", "API"],
+      github: "https://github.com/Prashu622",
+      
+    },
+    {
+      id: 7,
+      title: "PI - LEARNING PLATFORMS",
+      description:
+        "A React.js-based WEBSITE name PI mainly made for students or learner in this website i offered mainy courses like web dev or data science with including authentication system and also add payment summary.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript" , "Node.js" , "Express.js"],
+      github: "https://github.com/Prashu622",
+      
+    },
+    
+  ];  
+
+  // ...existing code...
+
+export const certificates = [
+  {
+    id: 1,
+    title: "Research Internship Certificate",
+    issuer: "MNNIT Allahabad",
+    date: "July 2025",
+    description: "Completed the Research Internship, covering real time project on AI/Ml and integrate on IOT, based device and advanced patterns.",
+    link: "https://drive.google.com/file/d/1EAYxqpVxW_ORKiKHjeCwp_dEICyJ3baA/view?usp=drivesdk"
+  },
+  {
+    id: 2,
+    title: "AI Tools Workshop",
+    issuer: "Be10x",
+    date: "January 2025",
+    description: "Earned the AI tools and ChatGpt Certified by Be10x, covering various AI tools and their applications.",
+    link: "https://drive.google.com/file/d/1ip08N5vn1dLQ640JMgfXpuXcS9IIxRzH/view?usp=drivesdk"
+  },
+  // Add more certificates as needed
+  {
+    id: 3,
+    title: "Coders Hunt Quiz",
+    issuer: "Digi Coders",
+    date: "March 2025",
+    description: "For successful participation in the quiz organised by the digicoders Technologies .",
+    link: "https://drive.google.com/file/d/1iiIPCqeV9mDQBkrxhNawZtwysk_zL6Gp/view?usp=drivesdk"
+  },
+  {
+    id: 4,
+    title: "Solution Architect (AWS Certified)",
+    issuer: "Forage",
+    date: "December 2024",
+    description: "Design the simple, scalable, hosting architecture using AWS.",
+    link: "https://drive.google.com/file/d/1ipCuB88h9R1BEjpxbV3NMxUkv5bWbiAo/view?usp=drivesdk"
+  },
+
+];
+// ...existing code...
+
